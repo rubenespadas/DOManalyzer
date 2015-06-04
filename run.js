6ac1c625c22d5f726636f7e1d01df3c797797a09
Rubén Espadas Vargas <ruben.espadas@PU050204>
2015-06-04 10:17:50 +0200

    Updated license texts.

--- run.js
+++ run.js
@@ -44,19 +44,15 @@ var argv = require('yargs')
     .default('s', './repository/samples')
     .default('r', './repository/reports')
     .count('verbose')
-    .epilog('DOManalyzer \n\
-    Copyright (C) 2015  Rubén Espadas \n\
-    \n\
+    .epilog('DOManalyzer - Copyright (C) 2015  Rubén Espadas \n\n\
     This program is free software: you can redistribute it and/or modify \n\
     it under the terms of the GNU General Public License as published by \n\
     the Free Software Foundation, either version 3 of the License, or \n\
-    (at your option) any later version. \n\
-    \
+    (at your option) any later version. \n\n\
     This program is distributed in the hope that it will be useful, \n\
     but WITHOUT ANY WARRANTY; without even the implied warranty of \n\
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n\
-    GNU General Public License for more details. \n\
-    \n\
+    GNU General Public License for more details. \n\n\
     You should have received a copy of the GNU General Public License \n\
     along with this program.  If not, see <http://www.gnu.org/licenses/>.\n')
     .argv;
